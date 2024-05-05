d599e9e330ad97db36b0d69537e9e67206e974af
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-05-05 23:50:37 +0530

    [sakshi]add : file io operations

--- FileIO/FileIO/Program.cs
+++ FileIO/FileIO/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace File
+{
+    internal class Fileio
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream("e:\\b.txt",FileMode.OpenOrCreate);
+            fileStream.WriteByte(65);
+            fileStream.Close();
+        }
+    }
+}
--- FileIO/FileIO/obj/Debug/net6.0/FileIO.AssemblyInfo.cs
+++ FileIO/FileIO/obj/Debug/net6.0/FileIO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf614ba214bf884553ad295bf17f407b5a3ae3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
